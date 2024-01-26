e1a68fb987eaccd598fbc1d025611bfa20513a29
Yogesh Basavaraju <w1853885@westminster.ac.uk>
2024-01-26 03:14:06 +0000

    complex nested structure

--- App.js
+++ App.js
@@ -3,16 +3,22 @@
 //      <h1></h1>
 //        <h2></h2> Adding Siblings
 //    </div>
+// <div id="child2">
+// <h1></h1>
+// <h2></h2> Adding Siblings //{' '}
+//</div>
 // </div>
 
-const parent = React.createElement(
-  'div',
-  { id: 'parent' },
+const parent = React.createElement('div', { id: 'parent' }, [
   React.createElement('div', { id: 'child' }, [
     React.createElement('h1', {}, 'hello World'),
     React.createElement('h2', {}, 'Thank you'),
-  ])
-);
+  ]),
+  React.createElement('div', { id: 'child2' }, [
+    React.createElement('h1', {}, 'hello World'),
+    React.createElement('h2', {}, 'Thank you'),
+  ]),
+]);
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 
