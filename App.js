50cae36d74ee7f1e0371503326426a973dcba389
Yogesh Basavaraju <w1853885@westminster.ac.uk>
2024-01-26 03:01:05 +0000

    Nested structure

--- App.js
+++ App.js
@@ -1,4 +1,19 @@
-const heading = React.createElement('h1', { id: 'heading' }, 'hello world');
+//  <div id= 'parent'>
+//    <div id ='child'>
+//      <h1></h1>
+//    </div>
+// </div>
+
+const parent = React.createElement(
+  'div',
+  { id: 'parent' },
+  React.createElement(
+    'div',
+    { id: 'child' },
+    React.createElement('h1', {}, 'hello World')
+  )
+);
+
 const root = ReactDOM.createRoot(document.getElementById('root'));
 
-root.render(heading);
+root.render(parent);
