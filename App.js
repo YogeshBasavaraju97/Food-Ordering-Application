53e453ee1e24e34326c83a5c603321d8b141061e
Yogesh Basavaraju <w1853885@westminster.ac.uk>
2024-01-26 03:04:37 +0000

    Adding siblings to nested structure

--- App.js
+++ App.js
@@ -1,17 +1,17 @@
 //  <div id= 'parent'>
 //    <div id ='child'>
 //      <h1></h1>
+//        <h2></h2> Adding Siblings
 //    </div>
 // </div>
 
 const parent = React.createElement(
   'div',
   { id: 'parent' },
-  React.createElement(
-    'div',
-    { id: 'child' },
-    React.createElement('h1', {}, 'hello World')
-  )
+  React.createElement('div', { id: 'child' }, [
+    React.createElement('h1', {}, 'hello World'),
+    React.createElement('h2', {}, 'Thank you'),
+  ])
 );
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
